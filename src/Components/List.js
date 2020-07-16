34a8ae0d161905c02bb3cad6db246e8c5bd79c19
RenjiraND <naufhalrenjira@gmail.com>
2020-07-16 16:38:30 +0700

    change list implementation for modal

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,53 +1,44 @@
 import React from 'react';
 import './list.scss';
 import { Issue } from '../Components/Issue';
-import { PlusIcon } from '@primer/octicons-react';
-import { Droppable } from 'react-beautiful-dnd'
+import { Droppable } from 'react-beautiful-dnd';
+import { Modal } from './Modal';
 
 export class List extends React.Component {
-
-  handleButton = e => {
-    console.log(e);
-  }
 
   render() {
     return (
       <div className="col-xl-4">
-        <div className="container card p-3 m-2">
-          <div className="container">
-            <div className="row">
-              <div className="col h4">
-                {this.props.column.title}
-              </div>
-              <div className="col-auto align-items-center">
-                <button className="add-task rounded-pill p-2" onClick={this.handleButton}>
-                  <span className="px-1">
-                    <PlusIcon size={14} className="align-middle" />
-                  </span>
-                  Add Task
-                </button>
+        <div className="card">
+          <div className="card-body">
+            <div className="container">
+              <div className="row">
+                <div className="col-auto h4">
+                  {this.props.column.title}
+                </div>
+                <Modal columnId={this.props.column.id} addTaskHandler={this.props.addTaskHandler}/>
               </div>
             </div>
+            <Droppable droppableId={this.props.column.id}>
+              {provided => (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                >
+                  {this.props.tasks.map((task, index) => {
+                    if(task !== undefined) {
+                      return (
+                        <Issue key={task.id} task={task} index={index} />
+                      );
+                    } else {
+                      return (null);
+                    }
+                  })}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
           </div>
-          <Droppable droppableId={this.props.column.id}>
-            {provided => (
-              <div
-                ref={provided.innerRef}
-                {...provided.droppableProps}
-              >
-                {this.props.tasks.map((task, index) => {
-                  if(task !== undefined) {
-                    return (
-                      <Issue key={task.id} task={task} index={index} />
-                    );
-                  } else {
-                    return (null);
-                  }
-                })}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
         </div>
       </div>
     );
