b48c979325ee6652cce0e8a929f71edea310de54
RenjiraND <naufhalrenjira@gmail.com>
2020-07-16 13:25:17 +0700

    implement sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,21 +1,60 @@
 import React from 'react';
 import './list.scss';
-import { SearchIcon, KebabHorizontalIcon } from '@primer/octicons-react';
+import { SearchIcon, KebabHorizontalIcon, PlusIcon } from '@primer/octicons-react';
+import { ReactComponent as Pic } from '../logo.svg';
+import { teamsData } from '../data';
 
 export class Sidebar extends React.Component {
 
+  teamsData = teamsData;
+
   render() {
     return (
-      <div class="sidenav text-white">
+      <div className="sidenav text-white">
         <Search />
         <Profile />
-        <div className="container">
+        <div className="container my-1">
           <div className="menu-dark p-2">
             Menu
           </div>
-          <a href="#">Home</a>
-          <a href="#">My Tasks</a>
-          <a href="#">Notifications</a> <span className="notif-count">3</span>
+          <div className="col container">
+            <div className="row link">Home</div>
+            <div className="row link">My Tasks</div>
+            <div className="row link">Notifications <span className="notif-count rounded-pill">3</span> </div> 
+          </div>
+        </div>
+
+        <div className="container my-1">
+          <div className="menu-dark p-2">
+            Teams
+          </div>
+          <div className="col container">
+            {this.teamsData.teams.map((teamId) => {
+              const team = this.teamsData.team[teamId]
+              return (
+                <div className="row">
+                  <div className="col-auto link">
+                    {team.name}
+                  </div>
+                  <div className="col">
+                    <div className="row justify-content-end">
+                      <Pic className="member rounded-circle"/>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+            <div className="row">
+              <button className="add-team p-2" onClick={this.handleButton}>
+                <div className="d-inline-block">
+                  <PlusIcon size={16} />
+                </div>
+                <div className="d-inline-block button-info px-1">
+                  Add Team
+                </div>
+              </button>
+            </div>
+          </div> 
         </div>
       </div>
     );
@@ -42,7 +81,7 @@ function Profile() {
     <div className="container profile py-2 px-4">
       <div className="row">
         <div className="col-auto">
-          <img src={imageLink} class="rounded-circle pic" alt="profile-pic" />
+          <img src={imageLink} className="rounded-circle pic" alt="profile-pic" />
         </div>
         <div className="col px-3">
           <div className="row name">
@@ -57,7 +96,7 @@ function Profile() {
         </div>
       </div>
 
-      <div className="d-flex task">
+      <div className="d-flex task my-2">
         <div className="col-6">
           <div className="row task-number">372</div>
           <div className="row task-info">Completed Tasks</div>
