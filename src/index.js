634a6b25d98b900ee7e8b9b9d6b70c61e46dc34d
RenjiraND <naufhalrenjira@gmail.com>
2020-07-16 16:35:51 +0700

    added bootstrap.js import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
